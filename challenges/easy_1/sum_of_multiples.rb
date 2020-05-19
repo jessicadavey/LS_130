634eca10cd39a2ea15c9f0d649132f7b4d068824
Jessica Davey <jessica@jessicadavey.com>
2020-05-19 15:06:46 -0500

    looked at solutions and refactored some more

--- challenges/easy_1/sum_of_multiples.rb
+++ challenges/easy_1/sum_of_multiples.rb
@@ -4,31 +4,27 @@ class SumOfMultiples
   end
 
   def to(limit)
-    self.class.to(limit, @divisors)
-  end
-
-  def self.to(limit, divisors=[3, 5])
     raise TypeError, "Must enter a positive integer" unless positive_integer?(limit)
-    range = (1...limit).to_a
-    range.select { |num| multiple?(num, divisors) }
+    (1...limit).to_a.select { |num| multiple?(num) }
          .sum
   end
 
-  def self.multiple?(num_to_check, divisors)
-    divisors.each do |divisor|
-      return true if num_to_check % divisor == 0
-    end
-    false
-  end
-
-  def self.positive_integer?(num)
-    num.instance_of?(Integer) && num > 0
+  def self.to(limit)
+    SumOfMultiples.new.to(limit)
   end
 
   private
 
+  def multiple?(num_to_check)
+    @divisors.any? { |divisor| num_to_check % divisor == 0 }
+  end
+
+  def positive_integer?(num)
+    num.instance_of?(Integer) && num > 0
+  end
+
   def valid_divisors?(numbers)
-    numbers.all? { |num|  self.class.positive_integer?(num)} &&
+    numbers.all? { |num| positive_integer?(num) } &&
       !numbers.empty?
   end
 end
