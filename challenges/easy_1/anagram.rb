fefd68790e822530b681aaa4e58f5aa863381e03
Jessica Davey <jessica@jessicadavey.com>
2020-05-21 09:10:28 -0500

    alternate solution to anagram challenge (not using sort)

--- challenges/easy_1/anagram.rb
+++ challenges/easy_1/anagram.rb
@@ -1,17 +1,17 @@
-class Anagram
-  def initialize(word)
-    @word = word
-  end
+# class Anagram
+#   def initialize(word)
+#     @word = word
+#   end
 
-  def match(potential_anagrams)
-    potential_anagrams.select { |word_to_check| anagram?(word_to_check) }
-  end
+#   def match(potential_anagrams)
+#     potential_anagrams.select { |word_to_check| anagram?(word_to_check) }
+#   end
 
-  def anagram?(word_to_check)
-    @word.downcase.chars.sort == word_to_check.downcase.chars.sort &&
-      @word.downcase != word_to_check.downcase
-  end
-end
+#   def anagram?(word_to_check)
+#     @word.downcase.chars.sort == word_to_check.downcase.chars.sort &&
+#       @word.downcase != word_to_check.downcase
+#   end
+# end
 
 =begin
 
@@ -20,7 +20,45 @@ Anagram rules:
 2. identical word is not an anagram
 3. anagrams are case insensitive
 
-
 input: array of words
 output: array of anagrams (subset of original array)
 =end
+
+# Alternate Solution (not using .sort):
+
+=begin
+
+Algorithm
+
+1. split both words into arrays of lowercase characters
+2. iterate through word1
+   - if second array has the character, delete it
+   - return false if it doesn't
+3. at the end, return true if the second word array is empty
+  and the words weren't the same word
+
+=end
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |word| anagram?(word, @word) }
+  end
+
+  private
+
+  def anagram?(word1, word2)
+    chars1 = word1.downcase.chars
+    chars2 = word2.downcase.chars
+
+    chars1.each do |char|
+      return false unless chars2.include?(char)
+      chars2.delete_at(chars2.index(char))
+    end
+
+    chars2.empty? && word1.downcase != word2.downcase
+  end
+end
